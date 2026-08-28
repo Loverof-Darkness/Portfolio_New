1496ab9ead0434a358b589408e652852c2200e9c
loverofdarkness <loverofdarkness@mail2indian.com>
2026-08-29 01:46:02 +0530

    Delete background-extras.js

--- background-extras.js
+++
@@ -1,2 +0,0 @@
-// Legacy loader intentionally disabled. The unified 16-background controller in background.js
-// selects exactly one background per dynamic reload and loads the appropriate core engine.
